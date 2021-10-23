5ffad6a4c912a294bb8dcb84043f1ecb99f1a2e0
Олег Шильников <shilnikovandoleg@gmail.com>
2021-10-23 21:01:27 +0600

    Добавьте файлы проекта.

---
+++ QRtoPDF/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using QRtoPDF.Models;
+using Rotativa.AspNetCore;
+using System.Diagnostics;
+
+
+namespace QRtoPDF.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+       
+        public IActionResult Index(string t)
+        {
+            
+           
+            return new ViewAsPdf("Index", Models.QRCode.GetOne(t));
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+      
+    }
+}
---
+++ QRtoPDF/Models/QRCode.cs
@@ -0,0 +1,29 @@
+﻿using QRCoder;
+using System.Drawing;
+
+
+namespace QRtoPDF.Models
+{
+    public class QRCode
+    {
+       
+        public string Qr { get; set; }
+  
+        public static QRCode GetOne(string t)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(t, QRCodeGenerator.ECCLevel.Q);
+            var imgType = Base64QRCode.ImageType.Jpeg;
+            Base64QRCode qrCode = new Base64QRCode(qrCodeData);
+            string qrCodeImageAsBase64 = qrCode.GetGraphic(20, Color.Black, Color.White, true, imgType);
+            
+            var code = new QRCode();
+            code.Qr = qrCodeImageAsBase64;
+            
+            
+            return code;
+        }
+    }
+   
+}
+
